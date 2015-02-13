06bcfb40c288cac437c956bcc7128c45dba3870c
Deepak <deepak@circarconsulting.com>
2015-02-13 14:48:42 +0530

    f

--- db/migrate/20150210144250_add_fields_to_doctor.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToDoctor < ActiveRecord::Migration
-  def change
-    add_column :doctors, :name, :string
-    add_column :doctors, :desgination, :string
-    add_column :doctors, :phone_no, :string
-    add_column :doctors, :address, :text
-    add_column :doctors, :doc_id, :string
-  end
-end
