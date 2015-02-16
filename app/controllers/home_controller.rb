aed71b890a1e4edefe954ff6fe8237926f624b28
Deepak <deepak@circarconsulting.com>
2015-02-16 10:23:16 +0530

    removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,19 +22,6 @@ def report
 
     #require 'rubygems'
     #require 'aws/s3'
-    
-   # AWS::S3::Base.establish_connection!( :access_key_id => "AKIAJ2RC5BMJYI2YWWXQ", :secret_access_key => "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")
-
-
-    # new_bucket = AWS::S3::Bucket.find('diagnostic123')
-     #new_bucket = AWS::S3::Bucket.find('my-new-bucket')
-     #new_bucket.each do |object|
-      #  puts "################"
-       # puts "#{object.key}\t#{object.about['content-length']}\t#{object.about['last-modified']}"
-     #end
-
-   # p new_bucket
-
     require 'nokogiri'
     #require 'open-uri'
     #url ="https://s3.amazonaws.com/diagnostic123/PATAPN.xml?X-Amz-Date=20150212T184813Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=4db1959ce488ed9c14145ef8e29b401ce54b81540ddb21a0436ed1fbe683d7cf&X-Amz-Credential=ASIAIRNKFPKTLWBHZODQ/20150212/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=AQoDYXdzEKr//////////wEakALmqit2xM/yDviudEJvkClR7W4ijtO2oHRwmiGMHe5G2HDnnx42/NA2zJsI0YlZtIu/mWz249ka8yY7Kb6LLkCGwR2jh0YcZkYvYIyt%2BGxdTzaSH/osYmj%2BMWIP2Xk1VE30CIOpcuIVZaqGskaIA2qCF43HYwGihB5mtNqOzGN%2BHjnmW/YRYWFUAeJGdA%2BZ/lKughZ2qjP5scAWKCey6rbnOhkmk84oQG3H62szDPjit5DFEe1gTcgre%2BYv1VE/xRzVLOFznnS0B2hMk%2BOPJdqIvO6u2Dtc2geJnVmh3%2BopzO%2BPpB0TuPBiARALfuaBpGjNw1Is8g%2BumYzz4TS3Ie3Btez9x06pMZEEiZXgDWBPIyCEv/OmBQ%3D%3D"
